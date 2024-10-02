a23a953b1dca32278f25b48d79c6e72bf51dee08
Ayush khatri <ayushkhatri773@gmail.com>
2024-10-02 14:01:36 +0530

    Create Superuser and Admin Panel Setup

--- LibraryManagementSystem/admin.py
+++ LibraryManagementSystem/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Book, Member, IssuedBook
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Member)
+admin.site.register(IssuedBook)
--- LibraryManagementSystem/models.py
+++ LibraryManagementSystem/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Book Model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    copies = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+# Member Model
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# IssuedBook Model (connects Book and Member)
+class IssuedBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    issue_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.book.title} issued to {self.member.name}'
