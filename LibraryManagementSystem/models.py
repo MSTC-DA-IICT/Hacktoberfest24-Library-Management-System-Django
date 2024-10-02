72cca8b84c85880083d93454a80a175eef2023a7
Spiritedswordsman <amanchandrakumar202@gmail.com>
2024-10-02 17:53:03 +0530

    added book+author models

--- LibraryManagementSystem/models.py
+++ LibraryManagementSystem/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('borrowed', 'Borrowed'),
+    ]
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    ISBN = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='available')
+
+    def __str__(self):
+        return f"{self.status} : {self.title} by {self.author.name}"
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
